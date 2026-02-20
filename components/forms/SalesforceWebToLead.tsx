fdedce362605bbbab814acbe1a7396d58359e9ac
Vaibhavnehwal2 <vaibhavdevops93@gmail.com>
2026-02-20 00:55:32 +0000

    auto-commit for b12f4afe-8920-435b-9d5c-03bda678663d

--- components/forms/SalesforceWebToLead.tsx
+++ components/forms/SalesforceWebToLead.tsx
@@ -583,13 +583,13 @@ export function SalesforceWebToLead({
               <CardDescription>{description || defaultDescription}</CardDescription>
             )}
           </div>
-          <div className="bg-gray-900 rounded-md px-2 py-1 shrink-0">
+          <div className="bg-gray-900 rounded-md px-2.5 py-1.5 shrink-0">
             <Image 
               src="/brand/knowmycrm-logo-large.png" 
               alt="KnowMyCRM" 
-              width={120} 
-              height={30}
-              className="h-7 w-auto"
+              width={140} 
+              height={35}
+              className="h-8 w-auto"
             />
           </div>
         </div>
